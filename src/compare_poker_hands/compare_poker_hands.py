3f314c37e9aacf19540706e82e2a1dd144cab1d8
LambdaXymox <lambda.xymox@gmail.com>
2018-10-27 19:06:02 -0500

    Write first test.

--- src/compare_poker_hands/compare_poker_hands.py
+++ src/compare_poker_hands/compare_poker_hands.py
@@ -1,3 +1,14 @@
+class PokerHand:
+    
+    RESULT = ["Loss", "Tie", "Win"]
+
+    def __init__(self, hand):
+        pass
+
+    def compare_with(self, other_hand):
+        pass
+
+
 def main():
     print('OMG IT RUNS FROM SETUP.PY!')
 
--- tests/test_compare_poker_hands.py
+++ tests/test_compare_poker_hands.py
@@ -1,6 +1,16 @@
 import pytest
 
+from compare_poker_hands.compare_poker_hands import PokerHand
 
-def test_should_autofail():
-    pytest.fail('You configured \"setup.py\" correctly, hooray!')
+
+def test_straight_flush_beats_four_of_a_kind():
+    """ 
+    Given: a straight flush hand and a four-of-a-kind hand.
+    Then: the straight flush hand wins.
+    """
+    straight_flush = PokerHand("2H 3H 4H 5H 6H") 
+    four_of_a_kind = PokerHand("AS AD AC AH JD")
+
+    assert straight_flush.compare_with(four_of_a_kind) == "Win"
+
 
