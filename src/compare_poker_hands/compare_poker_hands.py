f16b8922201b166700bc0ec9ce592db59e251c96
LambdaXymox <lambda.xymox@gmail.com>
2018-10-27 21:34:43 -0500

    Initial implementation of parse_poker_hand.

--- src/compare_poker_hands/compare_poker_hands.py
+++ src/compare_poker_hands/compare_poker_hands.py
@@ -1,13 +1,66 @@
+import enum
+
+from collections import namedtuple
+
+
+class Rank(enum.IntEnum):
+    TWO    = 2
+    THREE  = 3
+    FOUR   = 4
+    FIVE   = 5
+    SIX    = 6
+    SEVEN  = 7
+    EIGHT  = 8
+    NINE   = 9
+    TEN    = 10
+    JACK   = 11
+    QUEEN  = 12
+    KIND   = 13
+    ACE    = 14
+
+class Suit(enum.IntEnum):
+    SPADES   = 1
+    CLUBS    = 2
+    DIAMONDS = 3
+    HEARTS   = 4
+
+
+Card = namedtuple('Card', ['rank', 'suit'])
+
+
 def PokerHand(string):
     """
     Parse a poker hand string into an actual poker hand.
     """
-    cards = parse_poker_hand(string)
-    return Hand(cards)
+    return parse_poker_hand(string)
+
+
+def parse_card(card):
+    pass
 
 
 def parse_poker_hand(string):
-    return string.split(' ')
+    card_strings = string.split(' ')
+    if len(card_strings) != 5:
+        raise ValueError(
+            f'Hand must be exactly 5 cards. '
+            f'Got {string} with length {len(card_strings)}.'
+        )
+
+    cards = []
+    for card_string in card_strings:
+        try:
+            card = parse_card(card_string)
+        except:
+            raise ValueError(
+                f'Got an invalid poker hand string. Poker hands must have the'
+                f'form \"RS RS RS RS RS\", where \'R\' denotes a rank, '
+                f'and \'S\' denotes a suit.', string
+            )
+
+        cards.append(card)
+
+    return Hand(cards)
 
 
 class Hand:
@@ -17,11 +70,14 @@ class Hand:
     def __init__(self, hand):
         self.hand = hand
 
-    def compare_with(self, other_hand):
-        return "Win"
+    def compare_with(self, other):
+        return 'Win'
+
+    def __eq__(self, other):
+        return self.hand == other.hand
 
     def __repr__(self):
-        return f'PokerHand({self.hand[0]}, {self.hand[1]}, {self.hand[2]}, {self.hand[3]}, {self.hand[4]})'
+        return f'Hand([{self.hand[0]}, {self.hand[1]}, {self.hand[2]}, {self.hand[3]}, {self.hand[4]}])'
 
     def __str__(self):
         return f'{self.hand[0]} {self.hand[1]} {self.hand[2]} {self.hand[3]} {self.hand[4]}'
--- tests/test_compare_poker_hands.py
+++ tests/test_compare_poker_hands.py
@@ -1,16 +1,43 @@
 import pytest
 
-from compare_poker_hands.compare_poker_hands import PokerHand
+from compare_poker_hands.compare_poker_hands import PokerHand, Hand
+
+
+def test_poker_hand_factory_method_should_accept_valid_hand_strings():
+    """
+    GIVEN: A valid poker hand string, of the form 'RS RS RS RS RS', where
+           'R' denotes the rank of the card, and 'S' denotes the suit of the 
+           card.
+    WHEN:  We call the factory method passing the poker hand string.
+    THEN:  The factory method should correctly parse the string into a poker 
+           hand.
+    """
+    result = PokerHand('TS JC QD KH AH')
+    expected = Hand(['TS', 'JC', 'QD', 'KH', 'AH'])
+
+    assert result == expected
+
+
+def test_poker_hand_factory_method_should_reject_invalid_hand_strings():
+    """
+    GIVEN: A valid poker hand string has the form 'RS RS RS RS RS', where
+           'R' denotes the rank of the card, and 'S' denotes the suit of the 
+           card.
+    WHEN:  An invalid string is passed into the factory method.
+    THEN:  The factory method should reject the string as invalid.
+    """
+    with pytest.raises(ValueError):
+        result = PokerHand('potato potato potato potato')
 
 
 def test_straight_flush_beats_four_of_a_kind():
     """ 
-    Given: a straight flush hand and a four-of-a-kind hand.
-    Then: the straight flush hand wins.
+    GIVEN: A straight flush hand and a four-of-a-kind hand.
+    WHEN:  We compare the hands.
+    THEN:  The straight flush hand wins.
     """
-    straight_flush = PokerHand("2H 3H 4H 5H 6H") 
-    four_of_a_kind = PokerHand("AS AD AC AH JD")
+    straight_flush = PokerHand('2H 3H 4H 5H 6H') 
+    four_of_a_kind = PokerHand('AS AD AC AH JD')
 
-    assert straight_flush.compare_with(four_of_a_kind) == "Win"
-
+    assert straight_flush.compare_with(four_of_a_kind) == 'Win'
 
