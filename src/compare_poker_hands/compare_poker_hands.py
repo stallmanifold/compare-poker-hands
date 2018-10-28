65a38245b46d1a72e633ce4e44b541e99447a418
LambdaXymox <lambda.xymox@gmail.com>
2018-10-27 20:14:00 -0500

    First shot at implementing compare_with via TDD.

--- src/compare_poker_hands/compare_poker_hands.py
+++ src/compare_poker_hands/compare_poker_hands.py
@@ -1,12 +1,24 @@
-class PokerHand:
-    
+def PokerHand(string):
+    """
+    Parse a poker hand string into an actual poker hand.
+    """
+    cards = parse_poker_hand(string)
+    return Hand(cards)
+
+
+def parse_poker_hand(string):
+    return string.split(' ')
+
+
+class Hand:
+
     RESULT = ["Loss", "Tie", "Win"]
 
     def __init__(self, hand):
         self.hand = hand
 
     def compare_with(self, other_hand):
-        pass
+        return "Win"
 
     def __repr__(self):
         return f'PokerHand({self.hand[0]}, {self.hand[1]}, {self.hand[2]}, {self.hand[3]}, {self.hand[4]})'
