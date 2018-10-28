65f68c59db5bdfec7a5a9ef252046311f541d210
LambdaXymox <lambda.xymox@gmail.com>
2018-10-27 19:26:14 -0500

    Define string representations for PokerHand.

--- src/compare_poker_hands/compare_poker_hands.py
+++ src/compare_poker_hands/compare_poker_hands.py
@@ -3,10 +3,16 @@ class PokerHand:
     RESULT = ["Loss", "Tie", "Win"]
 
     def __init__(self, hand):
-        pass
+        self.hand = hand
 
     def compare_with(self, other_hand):
         pass
+
+    def __repr__(self):
+        return f'PokerHand({self.hand[0]}, {self.hand[1]}, {self.hand[2]}, {self.hand[3]}, {self.hand[4]})'
+
+    def __str__(self):
+        return f'{self.hand[0]} {self.hand[1]} {self.hand[2]} {self.hand[3]} {self.hand[4]}'
 
 
 def main():
